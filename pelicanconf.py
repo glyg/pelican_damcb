acbcc6466aae75111c1cf5bbfa443fce80dde888
Guillaume Gay <guillaume@mitotic-machine.org>
2014-10-22 10:59:38 +0200

    fix vimeo tag

--- pelicanconf.py
+++ pelicanconf.py
@@ -64,9 +64,10 @@ NOTEBOOK_DIR = 'downloads/notebooks'
 
 PLUGIN_PATHS = ['../Pelican/pelican-plugins/', ]
 PLUGINS = ['summary', 'liquid_tags.img', 'liquid_tags.video',
-'liquid_tags.youtube', 'render_math',
-'liquid_tags.include_code', 'liquid_tags.notebook',
-'liquid_tags.literal']
+           'liquid_tags.vimeo',
+           'liquid_tags.youtube', 'render_math',
+           'liquid_tags.include_code', 'liquid_tags.notebook',
+           'liquid_tags.literal']
 
 # CATEGORY_SAVE_AS = None
 DEFAULT_PAGINATION = 10
